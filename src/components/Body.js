ab910daa8b01334b628e704642ce1e0880130825
RishieVyas <rishievyas@gmail.com>
2024-09-16 23:55:18 -0400

    Enhanced search UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import restaurants from "../../data";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div className="body">
-      <div className="searchBar"> Search</div>
+      <div className="searchBar">
+        <input
+          type="text"
+          className="search-input"
+          value={searchText}
+          onChange={(val) => {
+            setSearchText(val.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            console.log("Search clicked");
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="restaurant-container">
         {restaurants.map((res) => (
           <RestaurantCard key={res.id} res={res} />
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,6 +1,6 @@
 const RestaurantCard = ({ res }) => {
   return (
-    <div id={res.id} className="restaurant-card">
+    <div key={res.id} className="restaurant-card">
       <img src={res.img} />
       <div className="restaurant-card-footer">
         <h4>{res.name}</h4>
