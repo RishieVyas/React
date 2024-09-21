a662f60b9255c398c2987f6cd4a52e4f2a077187
RishieVyas <rishievyas@gmail.com>
2024-09-21 16:42:49 -0400

    Declaring the state in class based component

--- src/components/User.js
+++ src/components/User.js
@@ -3,16 +3,23 @@ import React from "react";
 class User extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      srNumber: 1,
+    };
   }
 
   render() {
     const { name, location, contact } = this.props;
+    const { srNumber } = this.state;
 
     return (
       <div className="user-card">
-        <h2>Name: {name} </h2>
-        <p>Location: {location} </p>
-        <p>Contact: {contact} </p>
+        <h2>
+          {srNumber}: {name}
+        </h2>
+        <p> Location: {location} </p>
+        <p> Contact: {contact} </p>
       </div>
     );
   }
